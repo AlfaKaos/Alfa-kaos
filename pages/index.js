bfa11d95d6169a49a41362b40349caf5047e8e71
AlfaKaos <muttaqijenal@gmail.com>
2025-04-09 23:03:09 +0700

    Create index.js
    Tambah file index.js ke dalam folder pages untuk perbaikan error deploy.

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Selamat datang di Alfa Kaos</h1>
+      <p>Website masih dalam tahap pengembangan</p>
+    </div>
+  );
+}
+buat folder pages dan index.js)
