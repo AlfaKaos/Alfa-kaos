745647e8109884ca69871ca3c7c1d4f2b3a0ccba
AlfaKaos <muttaqijenal@gmail.com>
2025-04-10 09:04:54 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Selamat datang di Alfa Kaos</h1>
-      <p>Website masih dalam tahap pengembangan</p>
-    </div>
-  );
+  return <h1>Hello, Alfa Kaos!</h1>;
 }
-buat folder pages dan index.js)
